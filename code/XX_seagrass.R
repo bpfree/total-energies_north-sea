e7d987ab6db0737dc8d5e94a2f1328f776514403
Brian Free <bpfree@Mac-1117.cable.rcn.com>
2026-01-12 14:17:46 -0500

    seagrass script

---
+++ code/XX_seagrass.R
@@ -0,0 +1,101 @@
+##########################
+### XX. seagrass ###
+##########################
+
+# Clear environment
+rm(list = ls())
+
+# packages
+# install straight from GitHub (requires remotes, pak, or devtools)
+# install.packages("remotes")  # skip if already installed
+# remotes::install_local("~/dev/odp_sdkr", build = TRUE, build_vignettes = TRUE)
+# remotes::install_github("ropensci/worrms")
+
+# load packages
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(dplyr,
+               odp,
+               sf,
+               tidyr)
+
+# Commentary on R and code formulation:
+## ***Note: If not familiar with dplyr notation
+## dplyr is within the tidyverse and can use %>%
+## to "pipe" a process, allowing for fluidity
+## Can learn more here: https://style.tidyverse.org/pipes.html
+
+## Another  common coding notation used is "::"
+## For instance, you may encounter it as dplyr::filter()
+## This means use the filter function from the dplyr package
+## Notation is used given sometimes different packages have
+## the same function name, so it helps code to tell which
+## package to use for that particular function.
+## The notation is continued even when a function name is
+## unique to a particular package so it is obvious which
+## package is used
+
+#####################################
+#####################################
+
+# parameters
+### will need to define the geometry field (e.g., wkt_point)
+### boundary box for the study region
+bbox <- 'geometry within "POLYGON((-4.4454 50.9954, 12.0059 50.9954, 12.0059 61.0170, -4.4454 61.0170, -4.4454 50.9954))"'
+odp_data <- "7199f9bc-96ae-49d1-a814-df8c4bcc7552"
+
+#####################################
+#####################################
+
+# Set directories
+## Define data directory (as this is an R Project, pathnames are simplified)
+### Input directories
+
+### Output directories
+#### Analysis directories
+output_dir <- "data/b_intermediate_data"
+
+#####################################
+#####################################
+
+# set up ODP API
+## to find the API key or create one, navigate to: https://app.hubocean.earth/account
+### Client (API key can come from ODP_API_KEY)
+### API key saved in .Renvironment so retrieving through the Sys.getenv()
+### this is so the API key is not public
+odp_api_key <- Sys.getenv("odp_api_key")
+client <- odp::odp_client(api_key = odp_api_key)
+
+#####################################
+#####################################
+
+# load in dataset (see https://app.hubocean.earth/) -- ICES cetaceans surveys
+dataset <- client$dataset(odp_data)
+
+# generate table (defaults to the first table in the dataset)
+table <- dataset$table
+schema <- table$schema()
+
+# query -- by boundary box
+## returns a cursor that streams rows lazily
+cursor <- table$select(filter = bbox)
+
+# fetch table into a dataframe that you can use for analysis
+df <- cursor$dataframe()
+
+dim(df)
+names(df)
+View(df)
+str(df)
+
+##############
+
+
+
+##############
+
+
+
+##############
+
+# export data
+

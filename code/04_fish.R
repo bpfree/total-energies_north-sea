1395628e7187eea7e8cb82e917ba6416603136ac
Brian Free <brian.patrick.free@gmail.com>
2026-02-20 17:26:17 +0100

    fix packages list, provide context and notes on code, correct grouping comment

--- code/04_fish.R
+++ code/04_fish.R
@@ -8,10 +8,20 @@ rm(list = ls())
 # load packages
 if (!require("pacman")) install.packages("pacman")
 pacman::p_load(dplyr,
+               fs,
+               h3,
+               janitor,
+               jsonlite,
+               mapview,
+               mregions2,
                odp,
+               purrr,
+               readr,
+               rmapshaper,
                sf,
-               tidyr,
-               terra)
+               stringr,
+               terra,
+               tidyr)
 
 # Commentary on R and code formulation:
 ## ***Note: If not familiar with dplyr notation
@@ -50,9 +60,12 @@ output_dir <- "data/b_intermediate_data"
 data <- terra::rast(fs::path(data_dir, "fish_north-sea_richness_present_day", ext = "tiff"))
 
 study_area <- sf::st_read(dsn = fs::path(output_dir, "study_area.gpkg"), layer = "north_sea") %>%
+  # change to match coordinate reference system of data
   sf::st_transform(x = .,
+                   # CRS of the other data
                    crs = crs(data))
 
+# inspect the study area
 mapview::mapview(study_area)
 sf::st_layers(dsn = fs::path(output_dir, "study_area.gpkg"))
 
@@ -61,10 +74,13 @@ sf::st_layers(dsn = fs::path(output_dir, "study_area.gpkg"))
 
 mapview::mapview(data)
 
-# limit fish data to 
+# limit fish data to study region
 ns_fish <- data %>%
+  # crop raster data to study region
   terra::crop(x = .,
+              # define study region
               y = study_area,
+              # mask the data (so that only the study region is shown)
               mask = T)
 mapview::mapview(ns_fish)
 
@@ -77,24 +93,32 @@ ns_fish_poly <- ns_fish |>
   dplyr::rename("species_rich" = 1) %>%
   # create new layer
   dplyr::mutate("rescale" = (species_rich - min(species_rich)) / (max(species_rich) - min(species_rich))) |>
+  # add a little amount to minimum species to remove possibility of 0 values
   dplyr::mutate("rescale" = ifelse(rescale == 0.00,
+                                   # if met, add 0.01 to minimum
                                    rescale + 0.01,
+                                   # otherwise keep value
                                    rescale))
 
+# inspect data
 View(ns_fish_poly)
-
 mapview::mapview(ns_fish_poly)
 
+#####################################
+
+# load hexagonal grids
 hex_dir <- "data/b_intermediate_data/study_area.gpkg"
 hex_grid <- sf::st_read(dsn = hex_dir, layer = "ns_hexes_full")
 
+# get fish data for hexagonal grids
 region_data_hex <- hex_grid[ns_fish_poly, ] %>%
   # spatially join fish species richness values to North Sea hex cells
   sf::st_join(x = .,
               y = ns_fish_poly,
               join = st_intersects) %>%
+  # drop geometry for faster processing
   sf::st_drop_geometry() |>
-  # group by H3 index and layer name
+  # group by H3 index
   dplyr::group_by(h3_index) |>
   # return only distinct rows (remove duplicates)
   dplyr::summarize(fish_rich = max(rescale)) %>%
@@ -110,6 +134,7 @@ duplicates <- region_data_hex %>%
   # show distinct options
   dplyr::distinct()
 
+# join the summarized fish data to original hex grid
 region_data_hex_join <- hex_grid %>%
   dplyr::inner_join(x = .,
                    y = region_data_hex,
@@ -120,8 +145,17 @@ mapview::mapview(region_data_hex_join)
 #####################################
 #####################################
 
-terra::writeRaster(x = ns_fish, filename = fs::path(output_dir, "ns_fish.grd"), overwrite = T)
+# export data
+terra::writeRaster(x = ns_fish,
+                   # define filename
+                   filename = fs::path(output_dir, "ns_fish.grd"),
+                   # overwrite data
+                   overwrite = T)
+
 sf::st_write(obj = region_data_hex_join,
+             # destination
              dsn = "data/c_hex_data/data_ns_hex.gpkg",
+             # layer name
              layer = "fish_hex",
+             # append
              append = F)

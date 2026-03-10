59340dab9f6aee41da4c10b06e27eda50ff348fb
Brian Free <brian.patrick.free@gmail.com>
2026-03-10 15:50:32 +0100

    create plots of SDMs for protected areas of interest

--- code/14_protected-areas_seabirds.R
+++ code/14_protected-areas_seabirds.R
@@ -39,7 +39,7 @@ sf::st_layers(dsn = data_dir)
 #####################################
 
 bbox <- 'geometry within "POLYGON((-4.4454 50.9954, 12.0059 50.9954, 12.0059 61.0170, -4.4454 61.0170, -4.4454 50.9954))"'
-odp_data <- "6fdd5db8-4866-4634-9011-d7591ae7ddc4"
+odp_data <- "d3a69ed4-75c7-4db9-aef9-db1c39e51296"
 
 # set up ODP API
 ## to find the API key or create one, navigate to: https://app.hubocean.earth/account
@@ -74,6 +74,9 @@ str(df)
 
 #####################################
 
+# load North Sea data
+ns <- sf::st_read(dsn = fs::path(output_dir, "study_area.gpkg"), layer = "greater_north_sea")
+
 # clean data
 data <- df %>%
   # clean column names
@@ -81,38 +84,131 @@ data <- df %>%
   # convert the WKB geometry field to a more user friendly geometry field
   dplyr::mutate(geometry = sf::st_as_sfc(structure(as.list(geometry), class = "WKB"))) %>%
   # set CRS to WGS84
-  sf::st_as_sf(crs = 4326)
-
-mapview::mapview(data)
-
-#####################################
-
-# load North Sea data
-ns <- sf::st_read(dsn = fs::path(output_dir, "study_area.gpkg"), layer = "greater_north_sea")
-
-# greater North Sea protected areas
-region_data <- data %>%
+  sf::st_as_sf(crs = 4326) %>%
   # obtain only protected areas in the study area
   rmapshaper::ms_clip(target = .,
                       # clip object
                       clip = ns)
 
-# inspect data
-list(unique(region_data$category_name))
-mapview::mapview(region_data)
+mapview::mapview(data)
 
-#####################################
-#####################################
-
-protected_areas <- region_data %>%
-  dplyr::filter(grepl(pattern = "Doggerbank|Sydlige Nordsø|",
+protected_areas <- data %>%
+  dplyr::filter(grepl(pattern = "Doggerbank|Sydlige Nordsø",
                       # field
-                      x = site_name,
+                      x = sitename,
                       # is not case sensitive
                       ignore.case = T))
 
-mapview::mapview(doggerbank)
+mapview::mapview(protected_areas)
 
-sydlige_nordso <- region_data %>%
-  dplyr::filter(site_name == "Sydlige Nordsø")
-mapview::mapview(sydlige_nordso)
+#####################################
+
+# Doggerbank
+fulmarus_glacialis <- terra::rast(x = "data/b_intermediate_data/species/seabirds/taxonid_137195_maxent_scen_current.grd") %>%
+  # crop species raster
+  terra::crop(x = .,
+              # boundary box
+              y = protected_areas,
+              # mask the data
+              mask = T)
+plot(fulmarus_glacialis)
+
+larus_fuscus <- terra::rast(x = "data/b_intermediate_data/species/seabirds/taxonid_137142_maxent_scen_current.grd") %>%
+  # crop species raster
+  terra::crop(x = .,
+              # boundary box
+              y = protected_areas,
+              # mask the data
+              mask = T)
+plot(larus_fuscus)
+
+morus_bassanus <- terra::rast(x = "data/b_intermediate_data/species/seabirds/taxonid_148776_maxent_scen_current.grd") %>%
+  # crop species raster
+  terra::crop(x = .,
+              # boundary box
+              y = protected_areas,
+              # mask the data
+              mask = T)
+plot(morus_bassanus)
+
+rissa_tridactyla <- terra::rast(x = "data/b_intermediate_data/species/seabirds/taxonid_137156_ensemble_scen_current.grd") %>%
+  # crop species raster
+  terra::crop(x = .,
+              # boundary box
+              y = protected_areas,
+              # mask the data
+              mask = T)
+plot(rissa_tridactyla[[1]])
+
+uria_aalge <- terra::rast(x = "data/b_intermediate_data/species/seabirds/taxonid_137133_maxent_scen_current.grd") %>%
+  # crop species raster
+  terra::crop(x = .,
+              # boundary box
+              y = protected_areas,
+              # mask the data
+              mask = T)
+plot(uria_aalge)
+
+phoca_vitulina <- terra::rast(x = "data/b_intermediate_data/species/cetaceans/taxonid_137084_ensemble_scen_current.grd") %>%
+  # crop species raster
+  terra::crop(x = .,
+              # boundary box
+              y = protected_areas,
+              # mask the data
+              mask = T)
+plot(phoca_vitulina[[1]])
+
+phocoena_phocoena <- terra::rast(x = "data/b_intermediate_data/species/cetaceans/taxonid_137117_maxent_scen_current.grd") %>%
+  # crop species raster
+  terra::crop(x = .,
+              # boundary box
+              y = protected_areas,
+              # mask the data
+              mask = T)
+plot(phocoena_phocoena)
+
+# Sydlige Nordsø
+halichoerus_grypus <- terra::rast(x = "data/b_intermediate_data/species/cetaceans/taxonid_137080_maxent_scen_current.grd") %>%
+  # crop species raster
+  terra::crop(x = .,
+              # boundary box
+              y = protected_areas,
+              # mask the data
+              mask = T)
+plot(halichoerus_grypus)
+
+gavia_arctica <- terra::rast(x = "data/b_intermediate_data/species/seabirds/taxonid_137186_maxent_scen_current.grd") %>%
+  # crop species raster
+  terra::crop(x = .,
+              # boundary box
+              y = protected_areas,
+              # mask the data
+              mask = T)
+plot(gavia_arctica)
+
+gavia_stellata <- terra::rast(x = "data/b_intermediate_data/species/seabirds/taxonid_137188_maxent_scen_current.grd") %>%
+  # crop species raster
+  terra::crop(x = .,
+              # boundary box
+              y = protected_areas,
+              # mask the data
+              mask = T)
+plot(gavia_stellata)
+
+hydrocoloeus_minutus <- terra::rast(x = "data/b_intermediate_data/species/seabirds/taxonid_567449_maxent_scen_current.grd") %>%
+  # crop species raster
+  terra::crop(x = .,
+              # boundary box
+              y = protected_areas,
+              # mask the data
+              mask = T)
+plot(hydrocoloeus_minutus)
+
+melanitta_nigra <- terra::rast(x = "data/b_intermediate_data/species/seabirds/taxonid_137073_maxent_scen_current.grd") %>%
+  # crop species raster
+  terra::crop(x = .,
+              # boundary box
+              y = protected_areas,
+              # mask the data
+              mask = T)
+plot(melanitta_nigra)
